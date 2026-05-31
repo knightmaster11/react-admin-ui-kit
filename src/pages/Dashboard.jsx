3db77004f1818a4bbd86ea160786eee3a517a6a5
knightmaster11 <iwandatafile123@gmail.com>
2026-05-31 14:24:05 +0700

    feat: add admin layout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,11 @@
+import AdminLayout from "../layouts/AdminLayout";
+
 export default function Dashboard() {
   return (
-    <div>
-      Dashboard Page
-    </div>
-  )
+    <AdminLayout>
+      <h1 className="text-2xl font-bold p-5">
+        Dashboard
+      </h1>
+    </AdminLayout>
+  );
 }
