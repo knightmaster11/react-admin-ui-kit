3c062a038f09d6f7532d8123e508cee51f08f37e
knightmaster11 <iwandatafile123@gmail.com>
2026-06-02 21:28:46 +0700

    feat: add application routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Dashboard from "../pages/Dashboard";
+import Users from "../pages/Users";
+import Products from "../pages/Products";
+import Settings from "../pages/Settings";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </BrowserRouter>
   );
